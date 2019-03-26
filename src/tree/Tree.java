b9e1df0ed0399d29c641802ec919a0d965094fba
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-03-26 12:01:29 +0530

    Update Lis Code

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,16 +1,19 @@
 package tree;
 
 import algorithms.Replace.ReplaceNodeWithSumOfSomething;
+import dp.DynamicProgramming.LIS;
 import sampletree.SampleTrees.SampleTree;
 import treetraversals.TreeTraversals.TreeTraversal;
+import utility.Utilities.Utility;
 
 public class Tree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node<Integer> root = SampleTree.getSampleTree();
+		/*Node<Integer> root = SampleTree.getSampleTree();
 		Node<Integer> updatedRoot=ReplaceNodeWithSumOfSomething.replaceNodeWithSumUtil(root);
-		TreeTraversal.preOrderTraversal(updatedRoot);
+		TreeTraversal.preOrderTraversal(updatedRoot);*/
+		Utility.Print(LIS.lis(new int[] { 10, 22, 9, 33, 21, 50, 41, 60 }));
 	}
 
 }
